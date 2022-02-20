d375bd36a2962817d45156d7560a738a880216d2
federzvz <85709865+federzvz@users.noreply.github.com>
2022-02-19 21:44:05 -0300

    Nuevos servicios CRUD en entidad Employee: crear, actualizar/modificar y eliminar empleado.
    Se crearon nuevos servicios CRUD:
    - Se agregó el endpoint /api/employe/post que permite crear un nuevo Empleado.
    - Se agregó el endpoint /api/employe/{id} que permite actualziar/modificar los datos de un objeto ya creado con método HTTP PUT.
    - Se creó el endpoint /api/employe/delete/{id} que elimina el empleado según el ID especificado por parámetro.

--- src/main/java/com/javatechnolessons/demo/controller/EmployeController.java
+++ src/main/java/com/javatechnolessons/demo/controller/EmployeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -50,13 +51,67 @@ public class EmployeController {
     @GetMapping("/get/{employeeid}")
     public ResponseEntity<Employee> getEmployee(@PathVariable("employeeid") String employeeid){
         try{
-            Optional<Employee> employee = Optional.ofNullable(iEmployeeJpaRepository.findByEmployeeid(employeeid);
+            Optional<Employee> employee = Optional.ofNullable(iEmployeeJpaRepository.findByEmployeeid(employeeid));
             if(employee.isPresent()){
                 return new ResponseEntity<>(employee.get(),HttpStatus.OK);
             }
             return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
         }catch(Exception e){
             return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Crear un nuevo empleado.
+     * @param employee Objeto Empleado.
+     * @return retorna el objeto empleado creado.
+     */
+    @PostMapping("/post")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        try{
+            Employee employeeAux = iEmployeeJpaRepository.save(new Employee(employee.getFirstName(),
+                    employee.getLastName(),employee.getEmployeeid(),employee.getRole()));
+            return new ResponseEntity<>(employeeAux, HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Actualizar/Modificar empleado según el ID.
+     * @param employeeid Corresponde al ID autoincremental de la base de datos.
+     * @param employee Objeto del empleado con los campos modificados.
+     * @return retorna el Objeto con sus datos modificados.
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long employeeid, @RequestBody Employee employee){
+        try{
+            Optional<Employee> employeeData = iEmployeeJpaRepository.findById(employeeid);
+            if(employeeData.isPresent()) {
+                Employee employeeAux = employeeData.get();
+                employeeAux.setFirstName(employee.getFirstName());
+                employeeAux.setLastName(employee.getLastName());
+                employeeAux.setRole(employee.getRole());
+                return new ResponseEntity<>(iEmployeeJpaRepository.save(employeeAux), HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Eliminar un Empleado según su ID.
+     * @param id Id del empleado correspondiente al de la DB.
+     * @return Retorna un mensaje de éxito(OK: Code 200) o el mensaje de la excepción(Code 500).
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long id){
+        try{
+            iEmployeeJpaRepository.deleteById(id);
+            return new ResponseEntity<>("El empleado con id: "+id+" ha sido eliminado exitosamente.",HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.toString(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
